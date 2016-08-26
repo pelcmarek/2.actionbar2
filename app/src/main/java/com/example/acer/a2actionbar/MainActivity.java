093f2f404d8cc3b910308d6220f0b35b3008f0f1
pelcmarek <pelcmarek@yahoo.com>
2016-08-26 14:20:03 +0200

    second commit

--- app/src/main/java/com/example/acer/a2actionbar/MainActivity.java
+++ app/src/main/java/com/example/acer/a2actionbar/MainActivity.java
@@ -5,16 +5,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 
 public class MainActivity extends Activity {
+
+    private RadioGroup radioSlovickaGroup;
+    private RadioButton radioSlovickaButton;
+    private Button btnDisplay;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        radioSlovickaGroup = (RadioGroup) findViewById(R.id.radioSlovicka);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioSlovickaGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioSlovickaButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(MainActivity.this,
+                        radioSlovickaButton.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
